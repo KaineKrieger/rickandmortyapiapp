c9c4ec22c14ce93abd4353bedbff18857930af23
Horner_Adam <horna733@hsd.k12.or.us>
2024-02-29 13:57:23 -0800

    trying to commit the readme cus it didn't work before

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rickandmortyapiapp
+
+i don't quite know how i want to use it but im using the rick and morty character api 
+
+https://rickandmortyapi.com/api/
+
+that i found from the collection you gave us
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+# importing the requests function to do the getting api part
 import requests
-import json
+
+# base is the website for the api
 base = "https://rickandmortyapi.com/api/"
+
+# there are more endpoints i could make but for now its just character, check the website docs when you come back to make an app out of this
 endpoint = "character"
+
+# THIS DOESNT mean the url by itself its making the new url from the base while adding aditional endpoints, check docs for formatting
 url = base + endpoint
+
+# calling the api for data
 response = requests.get(url)
 
+
+# basic boolean to make sure it works.
 if response.ok:
     
     print(response.text)
